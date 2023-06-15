1a89037ed3a887e71e634411015fd0187a99f83b
wlzToT <1228393790@qq.com>
2023-06-15 17:01:42 +0800

    update

--- app/src/main/java/edu/buptsse/youchat/ChatActivity.kt
+++ app/src/main/java/edu/buptsse/youchat/ChatActivity.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.unit.sp
 import edu.buptsse.youchat.ui.theme.Gray3
 import edu.buptsse.youchat.ui.theme.Teal200
 import edu.buptsse.youchat.ui.theme.YouChatTheme
+import kotlinx.coroutines.launch
 import java.util.*
 
 class ChatActivity : ComponentActivity() {
@@ -40,51 +41,66 @@ class ChatActivity : ComponentActivity() {
         var i = 0
     }
 
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             YouChatTheme {
-                val chatTextFieldHeight = 60.dp
+                val drawerState = rememberBottomDrawerState(BottomDrawerValue.Closed)
+                BottomDrawer(
+                    drawerContent = {
+                        Column(Modifier.fillMaxWidth().height(200.dp)) {
 
-                Box(Modifier.fillMaxHeight()) {
-                    Column(Modifier.padding(bottom = chatTextFieldHeight + 5.dp).fillMaxHeight().background(Gray3)) {
-                        // 头部，聊天对象
-                        Card(Modifier.fillMaxWidth().background(Teal200)) {
-                            Box(Modifier.fillMaxWidth().background(Teal200)) {
-                                IconButton({ finish() }, Modifier.align(Alignment.CenterStart).padding(start = 5.dp)) {
-                                    Icon(Icons.Default.ArrowBack, null, tint = Color.White)
+                        }
+                    },
+                    drawerState = drawerState
+                ) {
+                    val chatTextFieldHeight = 60.dp
+
+                    Box(Modifier.fillMaxHeight()) {
+                        Column(
+                            Modifier.padding(bottom = chatTextFieldHeight + 5.dp).fillMaxHeight().background(Gray3)
+                        ) {
+                            // 头部，聊天对象
+                            Card(Modifier.fillMaxWidth().background(Teal200)) {
+                                Box(Modifier.fillMaxWidth().background(Teal200)) {
+                                    IconButton(
+                                        { finish() },
+                                        Modifier.align(Alignment.CenterStart).padding(start = 5.dp)
+                                    ) {
+                                        Icon(Icons.Default.ArrowBack, null, tint = Color.White)
+                                    }
+                                    Text(
+                                        name,
+                                        Modifier.align(Alignment.Center),
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 22.sp,
+                                        color = Color.White
+                                    )
                                 }
-                                Text(
-                                    name,
-                                    Modifier.align(Alignment.Center),
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 22.sp,
-                                    color = Color.White
-                                )
+                            }
+                            // 聊天内容
+                            LazyColumn(Modifier.fillMaxSize()) {
+                                msg.forEach { m ->
+                                    item { MessageBox(m.first, m.second) }
+                                }
                             }
                         }
-                        // 聊天内容
-                        LazyColumn(Modifier.fillMaxSize()) {
-                            msg.forEach { m ->
-                                item { MessageBox(m.first, m.second) }
-                            }
-                        }
-                    }
-                    // 编辑框
-                    Box(
-                        Modifier.fillMaxWidth().align(Alignment.BottomCenter)
-                            .height(chatTextFieldHeight)
-                    ) {
-                        var chatText by remember { mutableStateOf("") }
-                        var isSendButton by remember { mutableStateOf(false) }
-                        OutlinedTextField(
-                            value = chatText,
-                            onValueChange = {
-                                chatText = it
-                                isSendButton = chatText.isNotEmpty()
-                            },
-                            modifier = Modifier.padding(5.dp, 5.dp, 100.dp, 5.dp).fillMaxWidth(),
-                            /*leadingIcon = { Icon(painterResource(R.drawable.ic_baseline_search_24), contentDescription = null) },
+                        // 编辑框
+                        Box(
+                            Modifier.fillMaxWidth().align(Alignment.BottomCenter)
+                                .height(chatTextFieldHeight)
+                        ) {
+                            var chatText by remember { mutableStateOf("") }
+                            var isSendButton by remember { mutableStateOf(false) }
+                            OutlinedTextField(
+                                value = chatText,
+                                onValueChange = {
+                                    chatText = it
+                                    isSendButton = chatText.isNotEmpty()
+                                },
+                                modifier = Modifier.padding(5.dp, 5.dp, 100.dp, 5.dp).fillMaxWidth(),
+                                /*leadingIcon = { Icon(painterResource(R.drawable.ic_baseline_search_24), contentDescription = null) },
                             trailingIcon = {
                                 Button(
                                     onClick = { onClick() },
@@ -92,48 +108,54 @@ class ChatActivity : ComponentActivity() {
                                     modifier = Modifier.padding(top = 3.dp, bottom = 3.dp, end = 2.dp).width(80.dp)
                                 ) { Text("搜索", fontSize = 15.sp, modifier = Modifier.align(Alignment.CenterVertically)) }
                             },*/
-                            shape = RoundedCornerShape(50),
-                            colors = TextFieldDefaults.outlinedTextFieldColors(
-                                backgroundColor = MaterialTheme.colors.background,
-                                focusedBorderColor = MaterialTheme.colors.background,
-                                unfocusedBorderColor = MaterialTheme.colors.background,
-                                textColor = Color.Black,
-                            )
-                        )
-                        if (isSendButton) {
-                            Button(
-                                onClick = {
-                                    val message =
-                                        Message("wlz", "wcx", chatText.toByteArray(), Date(), Message.Type.TEXT)
-                                    msg.add(Pair(i % 2 == 0, message))
-                                    chatText = ""
-                                },
-                                modifier = Modifier.padding(10.dp, 5.dp).align(Alignment.CenterEnd).width(80.dp),
-                                // 是否能被点击
-                                enabled = true,
-                                // 来个圆角按钮
                                 shape = RoundedCornerShape(50),
-                                // 按钮背景颜色和主题背景颜色区分开，所以使用secondary
-                                colors = ButtonDefaults.buttonColors(
-                                    backgroundColor = MaterialTheme.colors.secondary,
-                                    contentColor = MaterialTheme.colors.onBackground
+                                colors = TextFieldDefaults.outlinedTextFieldColors(
+                                    backgroundColor = MaterialTheme.colors.background,
+                                    focusedBorderColor = MaterialTheme.colors.background,
+                                    unfocusedBorderColor = MaterialTheme.colors.background,
+                                    textColor = Color.Black,
                                 )
-                            ) {
-                                Text(text = "发送", color = Color.White, fontSize = 20.sp)
-                            }
-                        } else {
-                            OutlinedButton(
-                                onClick = {
-
-                                },
-                                modifier = Modifier.padding(5.dp, 5.dp).align(Alignment.CenterEnd).clip(CircleShape),
-                                shape = CircleShape,
-                                colors = ButtonDefaults.outlinedButtonColors(
-                                    backgroundColor = Color.White,
-                                    contentColor = Color.Black
-                                )
-                            ) {
-                                Icon(Icons.Default.Add, null)
+                            )
+                            if (isSendButton) {
+                                Button(
+                                    onClick = {
+                                        val message =
+                                            Message("wlz", "wcx", chatText.toByteArray(), Date(), Message.Type.TEXT)
+                                        msg.add(Pair(i % 2 == 0, message))
+                                        chatText = ""
+                                        isSendButton = false
+                                    },
+                                    modifier = Modifier.padding(10.dp, 5.dp).align(Alignment.CenterEnd).width(80.dp),
+                                    // 是否能被点击
+                                    enabled = true,
+                                    // 来个圆角按钮
+                                    shape = RoundedCornerShape(50),
+                                    // 按钮背景颜色和主题背景颜色区分开，所以使用secondary
+                                    colors = ButtonDefaults.buttonColors(
+                                        backgroundColor = MaterialTheme.colors.secondary,
+                                        contentColor = MaterialTheme.colors.onBackground
+                                    )
+                                ) {
+                                    Text(text = "发送", color = Color.White, fontSize = 20.sp)
+                                }
+                            } else {
+                                val scope = rememberCoroutineScope()
+                                OutlinedButton(
+                                    onClick = {
+                                        scope.launch {
+                                            drawerState.expand()
+                                        }
+                                    },
+                                    modifier = Modifier.padding(5.dp, 5.dp).align(Alignment.CenterEnd)
+                                        .clip(CircleShape),
+                                    shape = CircleShape,
+                                    colors = ButtonDefaults.outlinedButtonColors(
+                                        backgroundColor = Color.White,
+                                        contentColor = Color.Black
+                                    )
+                                ) {
+                                    Icon(Icons.Default.Add, null)
+                                }
                             }
                         }
                     }
--- app/src/main/java/edu/buptsse/youchat/main/MessageUI.kt
+++ app/src/main/java/edu/buptsse/youchat/main/MessageUI.kt
@@ -2,16 +2,18 @@ package edu.buptsse.youchat.main
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ListItem
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,34 +22,40 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import edu.buptsse.youchat.Message
 import edu.buptsse.youchat.domain.User
-import edu.buptsse.youchat.ui.theme.Gray3
 import edu.buptsse.youchat.ui.theme.Gray5
 import edu.buptsse.youchat.ui.theme.Teal200
 import edu.buptsse.youxuancheng.network.getUser
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
  * key是好友的id，value是和好友的聊天消息
  */
 val msgMap = mutableStateMapOf(
-    "10002" to mutableStateListOf<Pair<Boolean, Message>>(Pair(false, Message("武连增", "伍昶旭",
-        "This is a test".toByteArray(), Date(), Message.Type.TEXT)))
+    "10002" to mutableStateListOf<Pair<Boolean, Message>>(
+        Pair(
+            false, Message(
+                "武连增", "伍昶旭",
+                "This is a test".toByteArray(), Date(), Message.Type.TEXT
+            )
+        )
+    )
 )
 
 @Composable
 fun MessageUI(activity: MainActivity) {
     Column(Modifier.background(Color.White).fillMaxWidth()) {
         Column(Modifier.background(Teal200).fillMaxWidth()) {
-            Card(Modifier.align(Alignment.CenterHorizontally)) {
-                Text(
-                    "疼逊扣扣",
-                    fontSize = 22.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
+            Text(
+                "疼逊扣扣",
+                Modifier.align(Alignment.CenterHorizontally).padding(vertical = 8.dp),
+                fontSize = 23.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
 
-        LazyColumn(Modifier.fillMaxWidth().padding(bottom = 50.dp)) {
+        // TODO: bottom padding
+        LazyColumn(Modifier.fillMaxWidth().padding(bottom = 0.dp)) {
             msgMap.entries.forEach {
                 item { MessageListView(getUser(it.key), activity) }
             }
@@ -67,8 +75,18 @@ private fun MessageListView(friend: User, activity: MainActivity) {
             if (msgList.size != 0) {
                 Text(msgList.last().second.text, color = Gray5, fontSize = 19.sp)
             }
-        }) {
-        Text(friend.username, fontSize = 21.sp, fontWeight = FontWeight.Bold)
+        }
+    ) {
+        Box {
+            // 昵称
+            Text(friend.username, Modifier.align(Alignment.CenterStart), fontSize = 21.sp, fontWeight = FontWeight.Bold)
+            // 时间
+            if (msgList.size != 0) {
+                val date = msgList.last().second.time
+                val format = SimpleDateFormat("yyyy-MM-dd aa hh:mm", Locale.CHINESE)
+                Text(format.format(date), Modifier.align(Alignment.CenterEnd), color = Gray5, fontSize = 18.sp)
+            }
+        }
     }
     Divider(thickness = 2.dp)
 }
