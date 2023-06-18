c64d8612f404fdf55700dd7f04fb0f7e742a2229
V <72606776+ChaselWu@users.noreply.github.com>
2023-06-18 12:36:27 +0800

    update

--- YouChatServer/src/Server.java
+++ YouChatServer/src/Server.java
@@ -1,7 +1,9 @@
 import chat.ChatRunner;
+import user.UserSystemRunner;
 
 public class Server {
     public static void main(String[] args) {
+        UserSystemRunner.run();
         ChatRunner.run();
     }
 }
