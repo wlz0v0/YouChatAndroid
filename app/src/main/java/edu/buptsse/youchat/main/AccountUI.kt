56c417a20ad6d29f811912e53ec7b52a4efd76c9
V <72606776+ChaselWu@users.noreply.github.com>
2023-06-25 21:25:51 +0800

    Update AccountUI.kt

--- app/src/main/java/edu/buptsse/youchat/main/AccountUI.kt
+++ app/src/main/java/edu/buptsse/youchat/main/AccountUI.kt
@@ -15,50 +15,63 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import edu.buptsse.youchat.LoginActivity
 import edu.buptsse.youchat.R
 import edu.buptsse.youchat.ui.theme.Gray5
+import edu.buptsse.youchat.ui.theme.Teal200
 import edu.buptsse.youchat.util.UserConfig
 import java.util.regex.Pattern
 
-var curUser:edu.buptsse.youchat.domain.User = UserConfig.nobody
+var curUser: edu.buptsse.youchat.domain.User = UserConfig.nobody
 var token = ""
 val passwordPattern: Pattern = Pattern.compile("")
 val phoneNumberPattern: Pattern = Pattern.compile("")
 
 @Composable
 fun AccountUI(activity: MainActivity, navController: NavController) {
-    Column(Modifier.fillMaxWidth()) {
-        Card(Modifier.fillMaxWidth().height(100.dp), backgroundColor = Color.White) {
-            Box(Modifier.padding(10.dp).background(Color.White)) {
-                Column(
-                    Modifier.align(Alignment.CenterStart).clickable {
-                        if (curUser == UserConfig.nobody){
-                            activity.jumpFromMainToLogin()
+    Box(Modifier.padding(bottom = 60.dp).fillMaxHeight()) {
+        Column(Modifier.fillMaxWidth()) {
+            Column(Modifier.background(Teal200).fillMaxWidth()) {
+                Text(
+                    "我的信息",
+                    Modifier.align(Alignment.CenterHorizontally).padding(vertical = 8.dp),
+                    fontSize = 23.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Card(Modifier.fillMaxWidth().height(100.dp), backgroundColor = Color.White) {
+                Box(Modifier.padding(10.dp).background(Color.White)) {
+                    Column(
+                        Modifier.align(Alignment.CenterStart).clickable {
+                            if (curUser == UserConfig.nobody) {
+                                activity.jumpFromMainToLogin()
+                            }
                         }
+                    ) {
+                        Text(
+                            curUser.username,
+                            fontSize = 25.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(start = 10.dp, top = 10.dp)
+                        )
+                        Text(
+                            "ID: ${curUser.id}",
+                            fontSize = 19.sp,
+                            modifier = Modifier.padding(start = 10.dp, top = 5.dp),
+                            color = Gray5
+                        )
                     }
-                ) {
-                    Text(
-                        curUser.username,
-                        fontSize = 25.sp,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(start = 10.dp, top = 10.dp)
-                    )
-                    Text(
-                        "ID: ${curUser.id}",
-                        fontSize = 19.sp,
-                        modifier = Modifier.padding(start = 10.dp, top = 5.dp),
-                        color = Gray5
-                    )
-                }
-                IconButton(onClick = {}, Modifier.align(Alignment.CenterEnd)) {
-                    Icon(painterResource(R.drawable.ic_baseline_exit_to_app_24), null)
+                    IconButton(onClick = {}, Modifier.align(Alignment.CenterEnd)) {
+                        Icon(painterResource(R.drawable.ic_baseline_exit_to_app_24), null)
+                    }
                 }
             }
         }
-        Column(Modifier.height(10.dp)) { }
-        // 加好友
-        ListItem(Modifier.background(Color.White).clickable {
-            
-        }) { Text("添加好友", fontSize = 22.sp) }
+        TextButton(
+            onClick = { LoginActivity.loginState = false },
+            Modifier.align(Alignment.BottomCenter).padding(top = 15.dp)
+        ) {
+            Text("退出登录", color = Color.Red, fontSize = 23.sp, fontWeight = FontWeight.Bold)
+        }
     }
 }
